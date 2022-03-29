83beffa698113a81999deec150d3fa5c7c24dc73
Julian Boetto <julib_8724@hotmail.com>
2022-03-29 11:32:53 -0300

    Npm i e app rodando

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import useWebSocket from 'react-use-websocket'
 
 export default function App() {
+
+  const { lastJsonMessage } = useWebSocket(``, {
+
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testando!</Text>
       <StatusBar style="auto" />
     </View>
   );
