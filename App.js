dabb78192596546f3359afe9f29622cc310ba330
Julian Boetto <julib_8724@hotmail.com>
2022-03-29 12:05:12 -0300

    Botão para pesquisa funcionando

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import useWebSocket from 'react-use-websocket';
 import { useState } from 'react';
+import { Input, Text } from 'react-native-elements'
+import { Feather as Icon } from '@expo/vector-icons'
 
 export default function App() {
 
   const [data, setData] = useState({});
+  const [text, setText] = useState('BTCUSDT');
+  const [symbol, setSymbol] = useState('btcusdt');
 
-  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/btcusdt@ticker`, {
+  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol}@ticker`, {
     onMessage: () => {
       if(lastJsonMessage){
         setData(lastJsonMessage)
@@ -18,8 +22,23 @@ export default function App() {
     reconnectInterval: 3000
   })
 
+  const searchButton = <Icon.Button
+    name='search'
+    size={24}
+    color='black'
+    backgroundColor='transparent'
+    onPress={e => setSymbol(text.toLocaleLowerCase())}
+  />
+
   return (
     <View style={styles.container}>
+      <Text h1>CryptoWatch 1.0</Text>
+      <Input
+        autoCapitalize='characters'
+        leftIcon={<Icon name='dollar-sign' size={24} color='black' />}
+        rightIcon={searchButton}
+        onChangeText={setText}
+      />
       <Text>{JSON.stringify(data)}</Text>
       <StatusBar style="auto" />
     </View>
@@ -28,9 +47,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    marginTop: 40,
+    margin: 20,
+    alignContent: 'center'
   },
 });
