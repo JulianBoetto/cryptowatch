95e789d0eb098c48cc6eba4e4c4ae0ac62d9806e
Julian Boetto <julib_8724@hotmail.com>
2022-04-02 21:01:58 -0300

    Função para calcular o total invertido

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import useWebSocket from 'react-use-websocket';
 import { useState } from 'react';
 import { Input, Text } from 'react-native-elements'
 import { Feather as Icon } from '@expo/vector-icons'
@@ -9,33 +8,22 @@ import CoinDetailedScreen from './src/screens/CoinDetailsScreen';
 
 export default function App() {
 
-  const [data, setData] = useState({});
   const [text, setText] = useState('BTCUSDT');
   const [symbol, setSymbol] = useState('btcusdt');
-
-  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol}@ticker`, {
-    onMessage: () => {
-      if(lastJsonMessage){
-        setData(lastJsonMessage)
-      }
-    },
-    onError: (event) => alert(event),
-    shouldReconnect: () => true, // TENTA RECONECTAR
-    reconnectInterval: 3000
-  })
 
   const searchButton = <Icon.Button
     name='search'
     size={24}
     color='white'
     backgroundColor='transparent'
-    onPress={e => setSymbol(text.toLocaleLowerCase())}
+    onPress={e => setSymbol(text.replace(/ /g,''))}
   />
 
   return (
     <View style={styles.container}>
       <Text h1 style={styles.titulo}>CryptoWatch 1.0</Text>
       <Input
+        style={styles.titulo}
         autoCapitalize='characters'
         leftIcon={<Icon name='dollar-sign' size={24} color='white' />}
         rightIcon={searchButton}
@@ -44,21 +32,6 @@ export default function App() {
       <CoinDetailedScreen 
         symbol={symbol}
       />
-      {/* <View style={styles.linha}>
-        <Text style={styles.titulo}>{data.s}</Text>
-      </View>
-      <View style={styles.linha}>
-        <Text style={styles.rotulo}>Preço Atual:</Text>
-        <Text style={styles.conteudo}>{data.c}</Text>
-      </View>
-      <View style={styles.linha}>
-        <Text style={styles.rotulo}>Variação:</Text>
-        <Text style={styles.conteudo}>{data.P}%</Text>
-      </View>
-      <View style={styles.linha}>
-        <Text style={styles.rotulo}>Volume:</Text>
-        <Text style={styles.conteudo}>{data.v}</Text>
-      </View> */}
       <StatusBar style="auto" />
     </View>
   );
--- src/screens/CoinDetailsScreen.js
+++ src/screens/CoinDetailsScreen.js
@@ -3,15 +3,88 @@ import React, { useState } from 'react'
 import useWebSocket from 'react-use-websocket';
 import { Feather as Icon } from '@expo/vector-icons'
 import { Input, Text } from 'react-native-elements'
-
+import { btc } from '../valores-cripto/btc-usdt'
 
 export default function CoinDetailsScreen({symbol}) {
 
   const [data, setData] = useState({});
+  let historic = []
 
+  const historic_btc = [{
+    data: '03-02-2022',
+    amount: 0.00145012,
+    price: 39033.77
+    // 206,879.02
+    // 2022-02-03 13:01:01
+    // 5.3
+  },{
+    data: '05-02-2022',
+    amount: 0.00125524,
+    price: 45093.98
+    // 238998.119881457
+    // 5.3
+  },{
+    data: '25-02-2022',
+    amount: 0.00144612,
+    price: 40281.86
+    // 207,451.58
+    // 2022-02-25 15:48:35
+    // 5.15
+  },{
+    data: '17-03-2022',
+    amount: 0.00024,
+    price: 41280.02
+  },{
+    data: '17-03-2022',
+    amount: 0.00047,
+    price: 41793.06
+  }]
+
+  const historic_waves = [{
+    data: '07-03-2022',
+    amount: 0.94743762,
+    price: 22.05
+  },{
+    data: '01-04-2022',
+    amount: 0.2,
+    price: 51
+  }]
+
+  const historic_test = [{
+    data: '07-03-2022',
+    amount: 1,
+    price: 46000
+  }
+  ,{
+    data: '01-04-2022',
+    amount: 2,
+    price: 47000
+  }
+]
+
+  switch (symbol) {
+    case 'btcusdt':
+      historic = historic_btc
+      break
+    case 'wavesusdt':
+      historic = historic_waves
+      break
+    default:
+      historic = []
+  }
+
+  
   const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol}@ticker`, {
-    onMessage: () => {
+    onMessage: () => { 
       if(lastJsonMessage){
+        let diferencia = 0
+        historic.map(valor => {
+          let total = valor.amount * valor.price          
+          let totalActual = valor.amount * data.c
+          let diferenciaValor = totalActual - total
+          diferencia = diferencia + diferenciaValor
+        })
+        lastJsonMessage.diferencia = diferencia.toFixed(2)
         setData(lastJsonMessage)
       }
     },
@@ -27,19 +100,33 @@ export default function CoinDetailsScreen({symbol}) {
       </View>
       <View style={styles.linha}>
         <Text style={styles.rotulo}>Preço Atual:</Text>
-        <Text style={styles.conteudo}>{data.c}</Text>
+        <Text style={styles.conteudo}> {data.c}</Text>
       </View>
       <View style={styles.linha}>
         <Text style={styles.rotulo}>Variação:</Text>
-        <Text style={styles.conteudo}>{data.P}%</Text>
+        <Text style={data.P >= 0 ? styles.positivo : styles.negativo}> {data.P}%</Text>
+      </View>
+      {/* <View style={styles.linha}>
+        <Text style={styles.rotulo}>Volume:</Text>
+        <Text style={styles.conteudo}> {data.v}</Text>
+      </View> */}
+      {/* <View style={styles.linha}>
+        <Text style={styles.rotulo}>Total invertido (usdt):</Text>
+        <Text style={styles.conteudo}> {total}</Text>
+      </View> */}
+      <View style={styles.linha}>
+        <Text style={styles.rotulo}>Total actual (usdt):</Text>
+        <Text style={data.diferencia >= 0 ? styles.positivo : styles.negativo}> {JSON.stringify(btc)}</Text>
       </View>
       <View style={styles.linha}>
-        <Text style={styles.rotulo}>Volume:</Text>
-        <Text style={styles.conteudo}>{data.v}</Text>
+        <Text style={styles.rotulo}>Ganancia (usdt):</Text>
+        <Text style={data.diferencia >= 0 ? styles.positivo : styles.negativo}> u$s {data.diferencia}</Text>
       </View>
+      
     </View>
   )
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -59,6 +146,14 @@ const styles = StyleSheet.create({
   conteudo: {
     fontSize: 24,
     color: '#fff'
+  },
+  positivo: {
+    fontSize: 24,
+    color: '#00ff00'
+  },
+  negativo: {
+    fontSize: 24,
+    color: '#ff0000'
   },
   linha: {
     flexDirection: 'row',
